f9f66e1dceb2bc1cc528aefc177a7ba69458e8a6
JoacoSV <66373018+JoacoSV@users.noreply.github.com>
2021-04-04 20:16:42 -0300

    Auto reset

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 
 
 void printDirectory(File dir, int numTabs);
+void SendString (String str);
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -22,6 +23,7 @@ File myFile;
 boolean sendFile = false;
 boolean dataReady = false;
 String btData;
+String fileName;
 
 byte character;
 
@@ -30,27 +32,42 @@ int bufferIndex = 0;
 BluetoothSerial SerialBT;
 
 
+
+void BTcallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+  if(event == ESP_SPP_SRV_OPEN_EVT){
+    Serial.println("Client Connected");
+  }
+
+  if(event == ESP_SPP_CLOSE_EVT ){
+    Serial.println("Client disconnected");
+    
+    ESP.restart();
+  }
+}
+
+
 void setup() {
   Serial.begin(9600);
 
+/*
   Serial.print("Initializing SD card...");
   if (!SD.begin(5)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
-
+*/
 
   pinMode(22, OUTPUT);
 
-  File root = SD.open("/");
-  root.rewindDirectory();
-  printDirectory(root, 0); //Display the card contents
-  root.close();
+  //File root = SD.open("/");
+  //root.rewindDirectory();
+  //printDirectory(root, 0); //Display the card contents
+  //root.close();
 
 
-
-  SerialBT.begin("WDGTOYOTA_A125336"); //Bluetooth device name Max. 31 Bytes
+  SerialBT.register_callback(BTcallback);
+  SerialBT.begin("WDGFORKLIFT_A2234436"); //Bluetooth device name Max. 31 Bytes
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
@@ -64,40 +81,69 @@ void loop() {
   if (sendFile){
   
     sendFile = false;
-    Serial.println("Sending...");
+    Serial.print("Sending...");
 
     // re-open the file for reading:
-    myFile = SD.open("/config.json");
+
+    /*
+    myFile = SD.open(fileName);
     if (myFile) {
-      Serial.println("config.json:");
+      //Serial.println(fileName + ":");
       // read from the file until there's nothing else in it:
       while (myFile.available()) {
         character = myFile.read();
-        Serial.write(character);
-        //SerialBT.println(Serial.write(myFile.read()));
         SerialBT.write(character);
       }
       SerialBT.write('\n');
+      
+      Serial.println("Done!");
 
-    }else{Serial.println("File not found.");}
+    }
+    else{Serial.println("File not found.");}
+    */
+    Serial.println("File not found.");
+    SendString("FNF");
+
   }
 
 
+
+
+
+
+  //Test if there is data available to read from input buffer
   if (SerialBT.available()) {
     char character = SerialBT.read();
     btData+= character;
-    if (character == 10){dataReady = true;} 
-  }
-
-  if (dataReady){
-    dataReady = false;
-    digitalWrite(22,!digitalRead(22));
-    Serial.println(btData);
-    sendFile = true;
-    btData = "";
+    if (character == 10){
+      
+      if (btData.substring(0,2) =="cmd"){
+      }
+      else{
+        fileName = "/" + btData;
+        Serial.print("Received file: ");
+        Serial.println(fileName);
+        sendFile = true;
+      }
+      digitalWrite(22,!digitalRead(22));
+      btData ="";
+    } 
   }
 }
 
+
+
+
+void SendString (String str){
+
+  int strLength = str.length();
+  
+  for (int index = 0; index < strLength; index ++){
+    SerialBT.write(str[index]);
+  }
+
+  SerialBT.write('\n');
+}
 
 
 
